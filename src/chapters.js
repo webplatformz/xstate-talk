7cd82ef296736f8d3db56c46178a3e4c35ce68aa
r3dDoX <r3dDoX@users.noreply.github.com>
2022-03-15 17:11:35 +0100

    fix deep-linking

--- src/chapters.js
+++ src/chapters.js
@@ -25,8 +25,12 @@ function insertToc(toc) {
 function attachEventListenersToTocChapters(deck) {
   // attach event listeners to table of content chapter previews
   const tocChapters = document.querySelectorAll(".toc section");
-  const chapterDeckSlides = document.querySelectorAll(".reveal .slides > section.deck-slide");
-  const allToplevelSlides = document.querySelectorAll(".reveal .slides > section");
+  const chapterDeckSlides = document.querySelectorAll(
+    ".reveal .slides > section.deck-slide"
+  );
+  const allToplevelSlides = document.querySelectorAll(
+    ".reveal .slides > section"
+  );
   tocChapters.forEach((chapter, index) => {
     const chapterDeckSlide = chapterDeckSlides[index];
     if (!chapterDeckSlides) {
@@ -45,10 +49,19 @@ function attachEventListenersToTocChapters(deck) {
 }
 
 const combinedChapters = chapters.join("");
-document.querySelector(".slides-container").innerHTML = `<div class="reveal"><div class="slides">${combinedChapters}</div></div>`;
+document.querySelector(
+  ".slides-container"
+).innerHTML = `<div class="reveal"><div class="slides">${combinedChapters}</div></div>`;
 insertToc(toc);
 
+const [deepLinkHorizontalSlide, deepLinkVerticalSlide] = location.hash
+  .split("/")
+  .slice(1);
 const slideDeck = await setupDeck();
+if (deepLinkHorizontalSlide) {
+  slideDeck.slide(deepLinkHorizontalSlide, deepLinkVerticalSlide ?? 0);
+  hideToc();
+}
 attachEventListenersToTocChapters(slideDeck);
 
 // add hot reloading (in dev mode)
